f298a8b7d908d79828b58357857d4e03f7b3cb87
Charles Anderson <charlesandreanderson22@gmail.com>
2018-01-19 20:28:54 -0800

    media model

---
+++ models/Media.js
@@ -0,0 +1,16 @@
+const 
+    mongoose = require('mongoose'),
+    mediaSchema = new mongoose.Schema({
+        title: { type: String, required: true},
+        category: { type: String, required: true},
+        subCategory: { type: String, required: true},
+        content: { type: String },
+        recycle: { type: Boolean },
+        sell: { type: Boolean },
+        sendBack: { type: Boolean }
+    })
+//
+
+const Media = mongoose.model('Media', mediaSchema)
+
+export default Media
